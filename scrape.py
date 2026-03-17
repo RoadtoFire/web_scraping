48868952778685673a6d88a0e0e9e815cd201162
RoadtoFire <fire.jawwad@gmail.com>
2026-03-17 23:53:00 +0500

    Basic web scraper on a demo site

--- scrape.py
+++ scrape.py
@@ -1,16 +1,30 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
-with open("./practice_sites/index.html") as html_file:
-    soup = BeautifulSoup(html_file, "lxml")
+source = requests.get("https://books.toscrape.com/").text
 
+soup = BeautifulSoup(source, "lxml")
 
+csv_file = open("book_scraper.csv", "w")
+csv_writer = csv.writer(csv_file)
 
-for article in soup.find_all("div", class_="article"):
-    headline = article.h2.a.text
-    print(headline)
+csv_writer.writerow(["title", "cost", "availability"])
 
-    summary = article.p.text
-    print(summary)
+for article in soup.find_all("article"):
+    book_title = article.h3.a["title"].strip()
+    book_cost = article.find("p", class_="price_color").text.strip()
+    book_available = article.find("div", class_="product_price").find("p", class_="instock availability").text.strip()
 
+    print(book_title)
+    print(book_cost)
+    print(book_available)
     print()
+
+    csv_writer.writerow([book_title, book_cost, book_available])
+
+
+csv_file.close()
+
+
+
